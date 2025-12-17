c0d0a39a1e9820965f71e9e482c7ef5f4110a8d3
acyeow <andrewcyeow@gmail.com>
2025-12-17 15:45:18 -0800

    chore: create file for scraper config

---
+++ config/scraper_config.py
@@ -0,0 +1,10 @@
+RANKINGS_URL = 'https://badmintonranks.com/ranking/bwf'
+OUTPUT_DIR = 'ranking_data'
+
+EVENT_TYPES = [
+    'Men\'s Single',
+    'Women\'s Single', 
+    'Men\'s Double',
+    'Women\'s Double',
+    'Mixed Double'
+]
